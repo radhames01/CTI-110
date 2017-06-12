1fd780665df2c4c6d32090b915994f0cc6ad74a8
radhames01 <fleteper6617@student.faytechcc.edu>
2017-06-12 18:44:33 -0400

    Add files via upload

---
+++ Sales Prediction.py
@@ -0,0 +1,11 @@
+# Program that calculate the sales prediction
+# 12 June 2017
+# CTI-110 M2T1 - Sales Prediction
+# Radhames Flete Perez
+
+
+
+sales = int(input("Enter the projected amount of total sales.\n"))
+Aprofit = sales*0.23
+
+print("The Profict made from :",sales,"are:",Aprofit)
